96fcc04771a256c9a8dc42fb759dd708b1cec96a
Todd Birchard <toddbirchard@gmail.com>
2019-01-18 16:56:19 -0500

    dict to string

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main(request):
     # prepped = Request('POST', url=endpoint, headers=headers, data=data)
     # prep = prepped.prepare()
     # print(prep.url, prep.body, prep.headers, prep.method)
-    req = requests.post(url=endpoint, headers=json.dumps(headers), data=json.dumps(data))
+    req = requests.post(url=endpoint, headers=headers, data=data)
     response = req.content
     create_response = {
         'endpoint': json.dumps(endpoint),
